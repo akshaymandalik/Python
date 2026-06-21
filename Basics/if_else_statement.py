1cdd3586cae45364b8640c8e67e85add9bbb9a4d
akshaymandalik <amandalik.tech@gmail.com>
2026-06-21 16:55:57 +0530

    Modified if statement and added if_else block

---
+++ Basics/if_else_statement.py
@@ -0,0 +1,7 @@
+def hint_username(username):
+    if len(username)<3:
+        print("Invalid Username! Must be at least 3 characters long")
+    else:
+        print("Valid username, proceeding with registration")
+
+hint_username("akshay")
--- Basics/if_statement.py
+++ Basics/if_statement.py
@@ -2,4 +2,4 @@ def hint_username(username):
     if len(username)<3:
         print("Invalid username. Must at least 3 characters long")
 
-hint_username("aks")
+hint_username("ak")
